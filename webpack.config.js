d28b763401d8710e3fcd92adade2b0a769d93014
howdy39 <tatsuya.howdy@gmail.com>
2018-02-11 00:47:33 +0900

    fix: webpack-dev-server に対応

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,10 @@ module.exports = {
     entry: './src/js/app.js',
     output: {
         filename: 'bundle.js',
-        path: path.join(__dirname, 'public/js'),
-    }
+        path: path.join(__dirname, 'public'),
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'public'), // このディレクトリとoutput.pathは同じじゃないとダメらしい
+        port: 3000,
+    },
 };
